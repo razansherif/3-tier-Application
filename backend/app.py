8f2f42dad7af7afe8a0db10a6f300ff9d302f5cc
Razan Sherif <razansherif2005@gmail.com>
2026-08-24 18:57:57 +0300

    modify front end

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import psycopg2
+from psycopg2.extras import RealDictCursor
 app = Flask(__name__)
 CORS(app)
 def get_db_connection():
